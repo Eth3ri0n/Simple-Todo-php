38adb097d9df4a092a799d3234e6ea434a94e66d
Eth3ri0n <eth3rias@gmail.com>
2024-04-30 13:31:57 +0200

    Fix issue with removing todo items in remove-todo.php

--- remove-todo.php
+++ remove-todo.php
@@ -3,6 +3,7 @@ $filename = __DIR__ . '/data/todos.json'; // Get the file path
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS); // Filter the query string
 $id = $_GET['id'] ?? ''; // Get the id from the query string
 
+// Check if the id is set
 if ($id) {
     $todos = json_decode(file_get_contents($filename), true); // Get the todos
     $todoIndex = array_search($id, array_column($todos, 'id')); // Get the index of the todo item
