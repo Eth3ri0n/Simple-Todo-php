794c1b7f2282ef88eb1d39121cd9a62409e1c6d6
Thierryno Randrianarisaina <105993207+Eth3ri0n@users.noreply.github.com>
2024-04-30 12:44:05 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>2024 &copy; All rights reserved</p>
+</footer>
---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    rel="stylesheet">
+<!-- CSS -->
+<link rel="stylesheet" href="public/css/style.css">
+<!-- JavaScript -->
+<script src="public/js/index.js" defer></script>
